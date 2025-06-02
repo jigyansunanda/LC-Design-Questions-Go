61ac649b680247dbec0ad71178815c3f2423e194
jigyansunanda <nandajigyansu@gmail.com>
2025-06-02 09:15:11 +0530

    Add BSTIterator implementation and problem statement to README
    Signed-off-by: jigyansunanda <nandajigyansu@gmail.com>

---
+++ solutions/binary_search_tree_iterator/bst_iterator.go
@@ -0,0 +1,30 @@
+package binary_search_tree_iterator
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// TODO: BSTIterator Implementation
+type BSTIterator struct {
+}
+
+func Constructor(root *TreeNode) BSTIterator {
+
+}
+
+func (bstIterator *BSTIterator) Next() int {
+
+}
+
+func (bstIterator *BSTIterator) HasNext() bool {
+
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * obj := Constructor(root);
+ * param_1 := obj.Next();
+ * param_2 := obj.HasNext();
+ */
